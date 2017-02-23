c30e1cc23a20f4bcd1cca475b4d91073ae60fdba
Shark that walks like a man <rsimoes@gmail.com>
2017-02-23 01:07:48 -0600

    condense more

--- public/js/tech-tree.js
+++ public/js/tech-tree.js
@@ -15,7 +15,7 @@ let config = {
     collapsable: true
   }
 };
-let rootNode = {HTMLid: 'root', data: {tier: 0, subtier: 0}};
+let rootNode = {HTMLid: 'root', data: {tier: 0}};
 
 $(document).ready(function() {
   $.getJSON('techs.json', function(techData) {
@@ -36,18 +36,19 @@ $(document).ready(function() {
     });
 
     function isTier1_1(tech) {
-      return tech.data.tier == 1
-        && tech.data.subtier == 1
-        && tech.parent.data.tier == 0;
+      return tech.data.tier == 1 && tech.parent.data.tier == 0;
     }
 
     techs = techs.map(function(tech) {
       tech.text.name = tech.data.name;
       let tier = tech.data.tier > 0
-          ? ' (Tier ' + tech.data.tier + '-' + tech.data.subtier + ')'
+          ? ' (Tier ' + tech.data.tier + ')'
           : ' (Starting)';
       let cost = tech.data.tier > 0
           ? 'Base Cost: ' + tech.data.cost
+          : '';
+      let weight = tech.data.tier > 0
+          ? 'Base Weight: ' + tech.data.weight
           : '';
       let category = tech.data.category + tier + '\n';
       tech.text.title = cost
@@ -62,7 +63,7 @@ $(document).ready(function() {
           ? { HTMLid: tech.HTMLid + '-pseudoParent',
               parent: rootNode,
               pseudo: true,
-              data: { tier: 0, subtier: 0 } }
+              data: {tier: 0} }
           : techs.filter(function(candidate) {
             return candidate.HTMLid === parentKey;
           })[0];
@@ -71,45 +72,14 @@ $(document).ready(function() {
       let tierDifference = isTier1_1(tech)
           ? 0
           : tech.data.tier - tech.parent.data.tier;
-      let subtierDifference = isTier1_1(tech)
-          ? 0
-          : tech.data.subtier - tech.parent.data.subtier;
       let nestedTech = tech;
-      while ( ! isTier1_1(nestedTech)
-              && nestedTech.parent != rootNode
-              && (tierDifference > 0 ||
-                  (tech.data.tier > 0
-                   && tierDifference == 0 && subtierDifference > 1) ) ) {
-        if ( tierDifference > 0 ) {
-          if ( nestedTech.data.subtier == 1 ) {
-            var pseudo = {
-              HTMLid: nestedTech.HTMLid + '-pseudoParent',
-              parent: nestedTech.parent, pseudo: true,
-              data: { tier: nestedTech.data.tier - 1, subtier: 4 }
-            };
-            tierDifference--;
-            subtierDifference = tech.data.subtier - 4;
-          }
-          else {
-            var pseudo = {
-              HTMLid: nestedTech.HTMLid + '-pseudoParent',
-              parent: nestedTech.parent, pseudo: true,
-              data: { tier: nestedTech.data.tier,
-                      subtier: nestedTech.data.subtier - 1 }
-            };
-            subtierDifference --;
-          }
-        }
-        else {
-          var pseudo = {
-            HTMLid: nestedTech.HTMLid + '-pseudoParent',
-            parent: nestedTech.parent, pseudo: true,
-            data: { tier: nestedTech.data.tier,
-                    subtier: nestedTech.data.subtier - 1 }
-          };
-          subtierDifference--;
-        }
-
+      while ( tierDifference > 0 ) {
+        var pseudo = {
+          HTMLid: nestedTech.HTMLid + '-pseudoParent',
+          parent: nestedTech.parent, pseudo: true,
+          data: { tier: nestedTech.data.tier - 1 }
+        };
+        tierDifference--;
         nestedTech.parent = pseudo;
         nestedTech = pseudo;
       }
